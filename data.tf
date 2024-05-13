41c7ebc09e51c4f2cabccbe1e8e2a8e7fa345d31
abhilashasr21 <abhilashasr@microsoft.com>
2024-05-13 14:35:01 +0530

    structure update

---
+++ data.tf
@@ -0,0 +1,9 @@
+data "template_file" "disk_initializationdb" {
+  template = file("./scripts/DiskInitialization-DB.sh")
+  vars     = {}
+}
+
+data "azurerm_virtual_machine" "vm_ext" {
+  name                = var.linux_vm_01["name"]
+  resource_group_name = var.rg_1["name"]
+}
--- linux_ext.tf
+++ linux_ext.tf
@@ -14,12 +14,3 @@ resource "azurerm_virtual_machine_extension" "linux_extension" {
   }
 }
 
-data "template_file" "disk_initializationdb" {
-  template = file("./scripts/DiskInitialization-DB.sh")
-  vars     = {}
-}
-
-data "azurerm_virtual_machine" "vm_ext" {
-  name                = var.linux_vm_01["name"]
-  resource_group_name = var.rg_1["name"]
-}
