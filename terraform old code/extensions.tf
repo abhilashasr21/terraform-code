a55639396f300c1cd63760cd89364f2b2958b2b7
abhilashasr21 <abhilashasr@microsoft.com>
2024-05-23 20:06:02 +0530

    commiting old code

---
+++ terraform old code/extensions.tf
@@ -0,0 +1,60 @@
+#####################################################################################
+#                           Install VM Extension                                    #
+#####################################################################################
+
+/*resource "azurerm_virtual_machine_extension" "install_customscriptextensiondb" {
+ /* for_each = {
+    for k, v in var.vm_list : k => v
+    if v.vm_category == "database"
+  }
+  name                 = "CustomScriptExtension-DiskInitializationdb"
+  //virtual_machine_id   = azurerm_virtual_machine.windows_vm[each.key].id
+  virtual_machine_id = "/subscriptions/f4a270f4-c469-4215-bef6-b4abaea6815e/resourceGroups/az-rg-01/providers/Microsoft.Compute/virtualMachines/az-test-02"
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+ 
+  /*protected_settings = <<SETTINGS
+  {    
+    "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(data.template_file.disk_initializationdb.rendered)}')) | Out-File -filepath DiskInitialization-DB.ps1\" && powershell -ExecutionPolicy Unrestricted -File DiskInitialization-DB.ps1"
+  }
+  SETTINGS
+ 
+  depends_on = [
+    azurerm_virtual_machine_data_disk_attachment.data_disk_attachment
+  ]
+}*/
+
+/*data "template_file" "disk_initializationdb" {
+  template = file("../scripts/DiskInitialization-DB.ps1")
+  vars     = {}
+}*/
+#####################################################################################
+#                           Install VM Extension for Diagnostics                    #
+#####################################################################################
+
+/*resource "azurerm_virtual_machine_extension" "windows_vm_diagnostics" {
+  for_each = {
+    for k, v in var.vm_list : k => v
+    if lower(v.image_offer) == "windowsserver"
+  }
+  name                 = "WindowsVMDiagnostics"
+ // virtual_machine_id   = azurerm_virtual_machine.windows_vm[each.key].id
+ virtual_machine_id = ""
+  publisher            = "Microsoft.Azure.Diagnostics"
+  type                 = "IaaSDiagnostics"
+  type_handler_version = "1.22"
+ 
+  settings = templatefile("../../../monitoring/windows-vm-extension-iaasdiagnostics-config/diagnostics.json", {
+    //resource_id          = azurerm_virtual_machine.windows_vm[each.key].id
+    resource id = ""
+    //storage_account_name = var.vm_list[each.key].vm_diagnostics_storage_account_name
+    storage_account_name = ""
+  })
+ 
+ /* protected_settings = <<SETTINGS
+  {    
+    "storageAccountName": "${var.vm_list[each.key].vm_diagnostics_storage_account_name}"
+  }
+  SETTINGS
+}*/
---
+++ terraform old code/linux_ext.tf
@@ -0,0 +1,57 @@
+/*resource "azurerm_virtual_machine_extension" "linux_extension" {
+  name                 = "DiskInitializationLinux"
+  virtual_machine_id   = data.azurerm_virtual_machine.vm_ext.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
+  settings = jsonencode({
+    "commandToExecute" = data.template_file.disk_initializationdb.template
+  })
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "custom_script" {
+  name                 = "PythonVersion"
+  virtual_machine_id   = data.azurerm_virtual_machine.vm_ext.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.1"
+  settings = <<SETTINGS
+  {
+    "fileUris": ["./scripts/python.sh"],
+    "commandToExecute": "bash script.sh"
+  }
+  SETTINGS
+}
+*/
+resource "azurerm_virtual_machine_extension" "linux_vm_diagnostics" {
+  /*for_each = {
+    for k, v in var.vm_list : k => v
+    if lower(v.image_offer) == "windowsserver"
+  }*/
+  name                 = "LinuxVMDiagnostics"
+  virtual_machine_id   = data.azurerm_virtual_machine.vm_ext.id
+  publisher            = "Microsoft.Azure.Diagnostics"
+  type                 = "LinuxDiagnostic"
+  type_handler_version = "4.0"
+
+  settings = templatefile("./Diagnostic/diagnostics_linux.json", {
+    resource_id          = data.azurerm_virtual_machine.vm_ext.id
+    storage_account_name = var.storage_account_name
+  })
+
+  protected_settings = <<SETTINGS
+  {    
+    "storageAccountName": "${var.storage_account_name}"
+  }
+  SETTINGS
+}
+
+
+
+
+
