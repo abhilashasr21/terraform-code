6101856117e9211f0361409a281aabeadada5c1e
abhilashasr21 <abhilashasr@microsoft.com>
2024-05-23 19:21:15 +0530

    python extension update

--- scripts/python.sh
+++ scripts/python.sh
@@ -4,8 +4,9 @@
 
 # Ensure Python is installed
 sudo apt-get update
-sudo apt-get install -y python3
-
+sudo apt-get install -y python2
+sudo update-alternatives --remove-all python
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1
 # Install any necessary packages or configurations for diagnostics
 # Example: Install sysstat for performance monitoring
 sudo apt-get install -y sysstat
