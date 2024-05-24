c317b35dbf2658e8e56532c324facfe46fbeed2d
abhilashasr21 <abhilashasr@microsoft.com>
2024-05-24 23:21:06 +0530

    restructuring

--- data.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_virtual_machine" "u_vm" {
-  name                = "az-linux-02"
-  resource_group_name = var.rg_1["name"]
-}
-
-data "template_file" "disk_initializationdb" {
-  template = file("./scripts/DiskInitialization-DB.sh")
-  vars     = {}
-}
--- extension.tf
+++ extension.tf
@@ -1,4 +1,26 @@
+locals {
+  settings_content = jsondecode(file("./Diagnostic/diag.json"))
+  updated_settings = merge(local.settings_content, {
+    storage_account_name = var.storage_account_name
+    resource_id          = data.azurerm_virtual_machine.u_vm.id
+  })
+}
+
+data "azurerm_virtual_machine" "u_vm" {
+  name                = "az-linux-02"
+  resource_group_name = var.rg_1["name"]
+}
+
+data "template_file" "disk_initializationdb" {
+  template = file("./scripts/DiskInitialization-DB.sh")
+  vars     = {}
+}
+
 resource "azurerm_virtual_machine_extension" "OmsAgentForLinux" {
+  /*for_each = {
+    for k, v in var.vm_list : k => v
+    if lower(v.image_offer) == "linuxserver"
+  }*/
   name                       = "OmsAgentForLinux"
   virtual_machine_id         = data.azurerm_virtual_machine.u_vm.id
   publisher                  = "Microsoft.EnterpriseCloud.Monitoring"
@@ -12,37 +34,12 @@ resource "azurerm_virtual_machine_extension" "OmsAgentForLinux" {
   )
 }
 
-/*resource "azurerm_virtual_machine_extension" "linux_vm_diagnostics" {
-  for_each = {
+
+resource "azurerm_virtual_machine_extension" "linux_vm_diagnostics" {
+  /*for_each = {
     for k, v in var.vm_list : k => v
     if lower(v.image_offer) == "linuxserver"
-  }
-  name                 = "LinuxDiagnostic"
-  virtual_machine_id   = module.linux_vm_01.id
-  publisher            = "Microsoft.Azure.Diagnostics"
-  type                 = "LinuxDiagnostic"
-  type_handler_version = "3.0"
-
-  settings = templatefile("./Diagnostic/diagnostics_linux.json", {
-    resource_id          = module.linux_vm_01.id
-    storage_account_name = var.storage_account_name
-  })
-
-  protected_settings = <<SETTINGS
-  {    
-    "storageAccountName": "${var.storage_account_name}"
-  }
-  SETTINGS
-}
-*/
-locals {
-  settings_content = jsondecode(file("./Diagnostic/diag.json"))
-  updated_settings = merge(local.settings_content, {
-    storage_account_name = var.storage_account_name
-    resource_id          = data.azurerm_virtual_machine.u_vm.id
-  })
-}
-resource "azurerm_virtual_machine_extension" "linux_vm_diagnostics" {
+  }*/
   name                       = "LinuxDiagnostic"
   virtual_machine_id         = data.azurerm_virtual_machine.u_vm.id
   auto_upgrade_minor_version = true
