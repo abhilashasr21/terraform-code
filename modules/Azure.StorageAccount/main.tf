37b3fa52b16def82b8e3f17eb8aa3f78ebeae435
abhilashasr21 <abhilashasr@microsoft.com>
2024-09-06 19:36:07 +0530

    Adding code for file share

--- modules/Azure.StorageAccount/main.tf
+++ modules/Azure.StorageAccount/main.tf
@@ -1,8 +1,32 @@
 resource "azurerm_storage_account" "example" {
-  name                     = var.name
-  resource_group_name      = data.azurerm_resource_group.rg.name
-  location                 = data.azurerm_resource_group.rg.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
+  name                            = var.name
+  resource_group_name             = data.azurerm_resource_group.rg.name
+  location                        = data.azurerm_resource_group.rg.location
+  account_tier                    = var.account_tier
+  account_replication_type        = var.account_replication_type
   allow_nested_items_to_be_public = false
+  account_kind                    = "FileStorage"
+}
+
+#####################################################################################
+#                                 Fileshare creation                                #
+#####################################################################################
+
+/*resource "azurerm_storage_share" "fileshare" {
+  count                = length(var.fileshare_list) > 0 ? length(var.fileshare_list) : 0
+  storage_account_name = var.name
+  name                 = var.fileshare_list[count.index].name
+  access_tier          = var.fileshare_list[count.index].access_tier
+  enabled_protocol     = var.fileshare_list[count.index].enabled_protocol
+  quota                = var.fileshare_list[count.index].quota
+}*/
+
+
+resource "azurerm_storage_share" "example" {
+  for_each             = var.fileshare_list
+  name                 = each.value.name
+  storage_account_name = each.value.storage_account_name
+  quota                = each.value.quota # Optional, specify quota in GB
+  access_tier          = each.value.access_tier
+  enabled_protocol     = each.value.enabled_protocol
 }
--- modules/Azure.StorageAccount/variable.tf
+++ modules/Azure.StorageAccount/variable.tf
@@ -1,17 +1,60 @@
 variable "name" {
-  type = string
+  type    = string
+  default = "sadevtools3prodeus2"
 }
 
 variable "rg_name" {
-  type = string
+  type    = string
+  default = "Test-01"
 }
 
 variable "account_tier" {
-  type = string
-  default = "Standard"
+  type    = string
+  default = "Premium"
 }
 
 variable "account_replication_type" {
-  type = string
+  type    = string
   default = "LRS"
+}
+
+variable "fileshare_list" {
+  type = map(object({
+    name                 = string
+    storage_account_name = string
+    access_tier          = string
+    enabled_protocol     = string
+    quota                = number
+  }))
+  description = "List of file shares to be created."
+  default = {
+    /*share1 = {
+      name                 = "wus3fbatlassian2-bitbucket-prod"
+      storage_account_name = "sadevtools3prodeus2"
+      access_tier          = "Premium"
+      enabled_protocol     = "NFS"
+      quota                = 500
+    }*/
+    share2 = {
+      name                 = "wus3fbatlassian2-confluence-prod"
+      storage_account_name = "sadevtools3prodeus2"
+      access_tier          = "Premium"
+      enabled_protocol     = "NFS"
+      quota                = 600
+    }
+    share3 = {
+      name                 = "wus3fbatlassian2-jira-prod"
+      storage_account_name = "sadevtools3prodeus2"
+      access_tier          = "Premium"
+      enabled_protocol     = "NFS"
+      quota                = 500
+    }
+    share4 = {
+      name                 = "deployecomjenkins-prod"
+      storage_account_name = "sadevtools3prodeus2"
+      access_tier          = "Premium"
+      enabled_protocol     = "NFS"
+      quota                = 250
+    }
+  }
 }
